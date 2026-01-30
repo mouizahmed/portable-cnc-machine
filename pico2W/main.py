5c1bb7aba38c3f32bd2b7163514a9fd03b344ccb
farisnotclos <52431725+Farzin-Aliverdi@users.noreply.github.com>
2026-01-29 20:55:58 -0500

    Update main.py. Added basic state machine transitions that display on LCD

--- pico2W/main.py
+++ pico2W/main.py
@@ -1,4 +1,4 @@
-# main.py by Farzin Aliverdi
+# methods.py by Farzin Aliverdi
 # 2026/02/29
 # This file handles the main flow and control system of the Pico2W 
 
@@ -7,7 +7,9 @@ from machine import Pin
 
 #### VARIABLES ####
 count = 0
-display = ""
+state = 0
+display1 = ""
+display2 = ""
 button = Pin(15, Pin.IN, Pin.PULL_DOWN)
 
 #### ALL METHODS ####
@@ -17,27 +19,46 @@ def init():
     displayString(2,0,"")
     longDelay(1000)
     clearScreen()
-    displayString(1,0,"Team 40: Portable")
+    displayString(1,0,"Team 40:Portable")
     displayString(2,0,"CNC Machine")
     longDelay(1000) 
     clearScreen()
+    state = 1
     button.irq(button_handler)
 
 def button_handler(pin):
     global count
     print(count)
     count += 1
+    
+def forceStopAll():
+    pass
 
 #### INTERRUPTS ####
 button.irq(button_handler, Pin.IRQ_FALLING)
 
 init()
 while(True):
-    display = "Count: " + str(count)
-    displayString(1,0,"--------")
-    displayString(2,0, display)
-    #clearScreen()
-    print("MAIN: " + str(count))
-    #longDelay(500)
+    display1 = "State no." + str(state)
+    if (state == 1): # IDLE
+        display2 = "IDLE"
+    elif (state == 2): # EMERGENCY STOP
+        display2 = "EMERGENCY STOP"
+        forceStopAll()
+    elif (state == 3): # MACHINING
+        display2 = "MACHINING 10%"
+    elif (state == 4): # WARNING
+        display2 = "WARNING"
+    elif (state == 5):
+        display2 = "DATA TRANSFER"
+    elif (state == 6):
+        display2 = "NO LAPTOP FOUND"
     
-    #count += 1
+    clearScreen()
+    displayString(1,0, display1)
+    displayString(2,0, display2)
+    
+    longDelay(1000)
+    if (state == 6):
+        state = 0
+    state += 1
